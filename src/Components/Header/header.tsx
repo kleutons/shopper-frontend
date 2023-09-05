1137b3c4a202b334a5633b2e35ab01c7c20443bd
kleuton novais <106082564+kleutons@users.noreply.github.com>
2023-09-05 12:32:40 -0300

    add: Header

---
+++ src/Components/Header/header.tsx
@@ -0,0 +1,17 @@
+import { Header as HeaderComponent } from "./style"
+
+export default function Header(){
+    return(
+        <HeaderComponent>
+            <div className="container">
+                <img src="logo.webp" alt="Logo da Shopper"/>
+                <nav>
+                    <ul>
+                        <li><a href="/">Atualizar Preços</a></li>
+                        <li><a href="/products">Exibir Produtos</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </HeaderComponent>
+    )
+}
