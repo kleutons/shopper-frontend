abedd72444615a5a5e631c51c5dccc7b802fcd19
kleuton novais <106082564+kleutons@users.noreply.github.com>
2023-09-08 16:54:42 -0300

    feat: melhoramento css , menus e footer

--- src/Components/Header/header.tsx
+++ src/Components/Header/header.tsx
@@ -1,16 +1,14 @@
+import NavMenu from "../NacMenu/navMenu"
 import { Header as HeaderComponent } from "./style"
 
 export default function Header(){
     return(
         <HeaderComponent>
             <div className="container">
+                <a href="./">
                 <img src="logo.webp" alt="Logo da Shopper"/>
-                <nav>
-                    <ul>
-                        <li><a href="./">Atualizar Preços</a></li>
-                        <li><a href="/products">Exibir Produtos</a></li>
-                    </ul>
-                </nav>
+                </a>
+                <NavMenu />
             </div>
         </HeaderComponent>
     )
--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { useEffect, useState } from "react";
 
-const URL_API = 'http://localhost:4000/';
+const URL_API = 'http://localhost:4000';
 
 const api = axios.create({
     baseURL: URL_API
@@ -10,34 +10,48 @@ const api = axios.create({
 export function useFetchGet<T = unknown>(url: string){
     
     const  [data, setData] = useState<T | null>(null);
-    const  [error, setError] = useState<Error | null>(null);  
+    const  [error, setError] = useState<Error | Response | null>(null);  
 
     useEffect( () => {
-        const fetch = async () => {
-            //Axios
-            api.get(url)
-            .then(response => {
-                const fetchData = response.data;
-                setData(fetchData);
-            })
-            .catch(err => {
-                setError(err);
-            })
-        }
 
-        fetch();
+        const fetchUrl = URL_API+url;
+        fetch(fetchUrl).then(response => {
+            if(response.ok){
+                return response.json();
+            }else{
+                setError(response);
+            }
+           
+        }).then((data) =>{
+            setData(data)
+        }).catch((err) =>{
+            setError(err);
+        });
+
     }, [url])
 
     return { data, error }
 }
 
-export function useFetchPostForm<T = unknown>(url: string, formData: FormData){
+export function useFetchPostForm(url: string, formData: FormData){
+
+    interface CustomAxiosError extends AxiosError {
+        data?: { error: String };
+      }
+
+    interface CustomResponse extends Response {
+        data: String;
+    }
+
+      
     const [isRequestSent, setIsRequestSent] = useState(false);
-    const [data, setData] = useState<T | null>(null);
-    const [error, setError] = useState<Error | null>(null);
+    const [data, setData] = useState<CustomResponse | null>(null);
+    const [error, setError] = useState<CustomAxiosError | null>(null);
 
     useEffect( () => {
     if (isRequestSent) {
+        setData(null);
+
         const fetch = async () => {
             //Axios
             api.post(url, formData,{
@@ -46,11 +60,17 @@ export function useFetchPostForm<T = unknown>(url: string, formData: FormData){
                 }
             })
             .then(response => {
-                const fetchData = response.data;
-                setData(fetchData);
+                if(response.data){
+                    setData(response.data);
+                }
             })
-            .catch(err => {
-                setError(err);
+            .catch(err => {              
+                if(err.response){
+                    setError(err.response);
+                }else{
+                    setError(err);
+                }
+                              
             }).finally(() =>{
                 setIsRequestSent(false);
             })
@@ -64,5 +84,5 @@ export function useFetchPostForm<T = unknown>(url: string, formData: FormData){
         setIsRequestSent(true);
     };
 
-    return { data, setData, error, sendRequest }
+    return { data, setData, error, setError, sendRequest }
 }
